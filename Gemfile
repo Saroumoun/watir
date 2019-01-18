cc1049df36d7dc79669c5fc9db59137a00d4f8f2
Saroumoun <bastien.hiel@gmail.com>
2019-01-18 16:28:43 +0100

    added watir and googlesearch

--- Gemfile
+++ Gemfile
@@ -6,3 +6,4 @@ gem 'dotenv'
 gem "nokogiri", ">= 1.6.7.rc"
 gem 'watir'
 gem 'selenium-webdriver'
+gem 'launchy'
---
+++ lib/google_searcher.rb
@@ -0,0 +1,18 @@
+require 'launchy'
+
+
+
+def check_if_user_gave_input
+  abort("Veuillez rentrer une recherche après le nom du fichier") if ARGV.empty?
+end
+
+def get_search
+  return search = ARGV unless check_if_user_gave_input 
+end
+
+def do_search(searches)
+  search = searches.join('+')
+  Launchy.open("https://www.google.com/search?q=#{search}")
+end
+
+do_search(get_search)
